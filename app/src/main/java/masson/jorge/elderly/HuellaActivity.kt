a2b18a2a28e451efcb153b6f475677a4dfe5a5f5
Jorge <j_masson_reynoso@hotmail.com>
2020-05-02 18:32:29 -0700

    Layout Huella

---
+++ app/src/main/java/masson/jorge/elderly/HuellaActivity.kt
@@ -0,0 +1,12 @@
+package masson.jorge.elderly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HuellaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_huella)
+    }
+}
